e10c73e2a341853bfaa8a7f0d917d90783103f19
Randy Reddig <ydnar@shaderlab.com>
2014-09-24 19:43:49 -0700

    Chained API experiment.

--- hitch.go
+++ hitch.go
@@ -21,47 +21,51 @@ func New() *Hitch {
 }
 
 // Use installs one or more middleware in the Hitch request cycle.
-func (h *Hitch) Use(middleware ...func(http.Handler) http.Handler) {
+func (h *Hitch) Use(middleware ...func(http.Handler) http.Handler) *Hitch {
 	h.middleware = append(h.middleware, middleware...)
+	return h
 }
 
 // UseHandler registers an http.Handler as a middleware.
-func (h *Hitch) UseHandler(handler http.Handler) {
+func (h *Hitch) UseHandler(handler http.Handler) *Hitch {
 	h.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			handler.ServeHTTP(w, req)
 			next.ServeHTTP(w, req)
 		})
 	})
+	return h
 }
 
 // Handle registers a handler for the given method and path.
-func (h *Hitch) Handle(method, path string, handler http.Handler) {
+func (h *Hitch) Handle(method, path string, handler http.Handler) *Hitch {
 	h.Router.Handle(method, path, wrap(handler))
+	return h
 }
 
 // HandleFunc registers a func handler for the given method and path.
-func (h *Hitch) HandleFunc(method, path string, handler func(http.ResponseWriter, *http.Request)) {
+func (h *Hitch) HandleFunc(method, path string, handler func(http.ResponseWriter, *http.Request)) *Hitch {
 	h.Router.Handle(method, path, wrap(http.HandlerFunc(handler)))
+	return h
 }
 
 // Get registers a GET handler for the given path.
-func (h *Hitch) Get(path string, handler http.Handler) { h.Handle("GET", path, handler) }
+func (h *Hitch) Get(path string, handler http.Handler) *Hitch { return h.Handle("GET", path, handler) }
 
 // Put registers a PUT handler for the given path.
-func (h *Hitch) Put(path string, handler http.Handler) { h.Handle("PUT", path, handler) }
+func (h *Hitch) Put(path string, handler http.Handler) *Hitch { return h.Handle("PUT", path, handler) }
 
 // Post registers a POST handler for the given path.
-func (h *Hitch) Post(path string, handler http.Handler) { h.Handle("POST", path, handler) }
+func (h *Hitch) Post(path string, handler http.Handler) *Hitch { return h.Handle("POST", path, handler) }
 
 // Patch registers a PATCH handler for the given path.
-func (h *Hitch) Patch(path string, handler http.Handler) { h.Handle("PATCH", path, handler) }
+func (h *Hitch) Patch(path string, handler http.Handler) *Hitch { return h.Handle("PATCH", path, handler) }
 
 // Delete registers a DELETE handler for the given path.
-func (h *Hitch) Delete(path string, handler http.Handler) { h.Handle("DELETE", path, handler) }
+func (h *Hitch) Delete(path string, handler http.Handler) *Hitch { return h.Handle("DELETE", path, handler) }
 
 // Options registers a OPTIONS handler for the given path.
-func (h *Hitch) Options(path string, handler http.Handler) { h.Handle("OPTIONS", path, handler) }
+func (h *Hitch) Options(path string, handler http.Handler) *Hitch { return h.Handle("OPTIONS", path, handler) }
 
 // ServeHTTP implements http.Handler.
 func (h *Hitch) ServeHTTP(w http.ResponseWriter, req *http.Request) {
